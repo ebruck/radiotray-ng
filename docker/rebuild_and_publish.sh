2507064491b72ee9400ca9fc0362ef7407b17328
Dmitry Afanasiev <afanasiev.dmitry@gmail.com>
2019-04-26 20:15:53 +0300

    +support ubuntu 16.04

--- docker/rebuild_and_publish.sh
+++ docker/rebuild_and_publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xe
 
+docker build -t radiotrayng/circleci:ubuntu-16.04 - <ubuntu/16.04/Dockerfile
+docker push radiotrayng/circleci:ubuntu-16.04
+
 docker build -t radiotrayng/circleci:ubuntu-18.04 - <ubuntu/18.04/Dockerfile
 docker push radiotrayng/circleci:ubuntu-18.04
 
