64f00b952468b6ab7a6832f74e443a6e028a1037
Dmitry Afanasiev <afanasiev.dmitry@gmail.com>
2019-06-17 15:48:37 +0300

    drop ubuntu 18.10

--- docker/rebuild_and_publish.sh
+++ docker/rebuild_and_publish.sh
@@ -7,9 +7,6 @@ docker push radiotrayng/circleci:ubuntu-16.04
 docker build -t radiotrayng/circleci:ubuntu-18.04 - <ubuntu/18.04/Dockerfile
 docker push radiotrayng/circleci:ubuntu-18.04
 
-docker build -t radiotrayng/circleci:ubuntu-18.10  - <ubuntu/18.10/Dockerfile
-docker push radiotrayng/circleci:ubuntu-18.10
-
 docker build -t radiotrayng/circleci:ubuntu-19.04 - <ubuntu/19.04/Dockerfile
 docker push radiotrayng/circleci:ubuntu-19.04
 
