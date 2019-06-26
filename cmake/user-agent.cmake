7b11e3d727857543cee74fa671ede81b9bbf0c61
Ed Bruck <ed.bruck1@gmail.com>
2019-06-26 11:57:41 -0700

    Build no longer fails if lsb_release is missing
    Fixes #116

--- cmake/user-agent.cmake
+++ cmake/user-agent.cmake
@@ -11,7 +11,7 @@ else()
         set(USER_AGENT_DISTRO_CODENAME "El Capitan")
         execute_process(COMMAND sw_vers -productVersion OUTPUT_VARIABLE USER_AGENT_DISTRO_RELEASE  OUTPUT_STRIP_TRAILING_WHITESPACE)
     else()
-        message(FATAL_ERROR "lsb_release not found!")
+        message(AUTHOR_WARNING "lsb_release not found!")
     endif()
 endif()
 
@@ -27,10 +27,16 @@ if (NOT RTNG_GIT_VERSION)
     set(RTNG_GIT_VERSION "v${PROJECT_VERSION}-unknown")
 endif()
 
-set(RTNG_USER_AGENT "${CMAKE_PROJECT_NAME}/${PROJECT_VERSION} (${CMAKE_SYSTEM_NAME} ${CMAKE_SYSTEM_PROCESSOR}; ${USER_AGENT_DISTRO}/${USER_AGENT_DISTRO_RELEASE} (${USER_AGENT_DISTRO_CODENAME}))")
+if (USER_AGENT_DISTRO AND USER_AGENT_DISTRO_RELEASE AND USER_AGENT_DISTRO_CODENAME)
+    set(RTNG_USER_AGENT "${CMAKE_PROJECT_NAME}/${PROJECT_VERSION} (${CMAKE_SYSTEM_NAME} ${CMAKE_SYSTEM_PROCESSOR}; ${USER_AGENT_DISTRO}/${USER_AGENT_DISTRO_RELEASE} (${USER_AGENT_DISTRO_CODENAME}))")
+else()
+    set(RTNG_USER_AGENT "${CMAKE_PROJECT_NAME}/${PROJECT_VERSION} (${CMAKE_SYSTEM_NAME} ${CMAKE_SYSTEM_PROCESSOR}; Uknown))")
+endif()
+
+
 configure_file(${PROJECT_SOURCE_DIR}/cmake/rtng_user_agent.hpp.in ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/rtng_user_agent.hpp.tmp)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/rtng_user_agent.hpp.tmp ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/rtng_user_agent.hpp)
 execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/rtng_user_agent.hpp.tmp)
 
-#message(STATUS "user-agent: ${RTNG_USER_AGENT}")
+# message(STATUS "user-agent: ${RTNG_USER_AGENT}")
