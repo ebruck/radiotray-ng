3ce71a051806960a84f8e83c0dc27417a58defb8
Ed Bruck <ed.bruck1@gmail.com>
2019-10-07 18:22:51 -0700

    added Ubuntu 19.10 & Fedora 31 builds

--- docker/rebuild_and_publish.sh
+++ docker/rebuild_and_publish.sh
@@ -11,5 +11,11 @@ docker push radiotrayng/circleci:ubuntu-18.04
 docker build -t radiotrayng/circleci:ubuntu-19.04 - <ubuntu/19.04/Dockerfile
 docker push radiotrayng/circleci:ubuntu-19.04
 
+docker build -t radiotrayng/circleci:ubuntu-19.10 - <ubuntu/19.10/Dockerfile
+docker push radiotrayng/circleci:ubuntu-19.10
+
 docker build -t radiotrayng/circleci:fedora-30 - <fedora/30/Dockerfile
 docker push radiotrayng/circleci:fedora-30
+
+docker build -t radiotrayng/circleci:fedora-31 - <fedora/31/Dockerfile
+docker push radiotrayng/circleci:fedora-31
