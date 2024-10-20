63dd556e73c7d8a50e9caa610634858e5d342723
Ed Bruck <ed.bruck1@gmail.com>
2024-10-20 11:55:35 -0700

    * removed EOL distros from packaging * added 24.04 & removed Fedora build * disabled deprecated warnings (for now) * cleanup

--- docker/rebuild_and_publish.sh
+++ docker/rebuild_and_publish.sh
@@ -2,14 +2,11 @@
 
 set -xe
 
-docker build -t radiotrayng/circleci:ubuntu-18.04 - <ubuntu/18.04/Dockerfile
-docker push radiotrayng/circleci:ubuntu-18.04
-
 docker build -t radiotrayng/circleci:ubuntu-20.04 - <ubuntu/20.04/Dockerfile
 docker push radiotrayng/circleci:ubuntu-20.04
 
 docker build -t radiotrayng/circleci:ubuntu-22.04 - <ubuntu/22.04/Dockerfile
 docker push radiotrayng/circleci:ubuntu-22.04
 
-docker build -t radiotrayng/circleci:fedora-34 - <fedora/34/Dockerfile
-docker push radiotrayng/circleci:fedora-34
+docker build -t radiotrayng/circleci:ubuntu-24.04 - <ubuntu/24.04/Dockerfile
+docker push radiotrayng/circleci:ubuntu-24.04
